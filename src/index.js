2d8dba1248477f9b4261ce5a6205c8958af948fb
santhosh274 <santhosh.k@rapyder.com>
2019-07-15 22:53:37 +0530

    sending img src props from parent to child

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className = "ui container comments">
-            <CommentDetail author={faker.name.firstName()} />
-            <CommentDetail author={faker.name.firstName()} />
-            <CommentDetail author={faker.name.firstName()} />
-            <CommentDetail author={faker.name.firstName()} />
+            <CommentDetail author={faker.name.firstName()} timeAgo="today at 9:00 PM"  comments={faker.random.words()} imageSource={faker.image.avatar()} />
+            <CommentDetail author={faker.name.firstName()} timeAgo="yesterday at 4:00 PM" comments={faker.random.words()} imageSource={faker.image.avatar()} />
+            <CommentDetail author={faker.name.firstName()} timeAgo="today at 10:00 AM" comments={faker.random.words()} imageSource={faker.image.avatar()}/>
+            <CommentDetail author={faker.name.firstName()} timeAgo={faker.random.number()} comments={faker.random.words()} imageSource={faker.image.avatar()} />
         </div>
     );
 }
