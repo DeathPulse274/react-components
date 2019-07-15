95273fbc47cf47162c78c7fc891ca4874c34e239
santhosh274 <santhosh.k@rapyder.com>
2019-07-15 23:01:54 +0530

    Updates

---
+++ precache-manifest.d123681634e2b199435a8597390f1227.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d03a8b1d554014074668af84459801",
+    "url": "/DeathPulse274/react-components/index.html"
+  },
+  {
+    "revision": "92cda067c4bec7226b50",
+    "url": "/DeathPulse274/react-components/static/js/2.4d70910c.chunk.js"
+  },
+  {
+    "revision": "9dd6bedd0dee5c270a54",
+    "url": "/DeathPulse274/react-components/static/js/main.af350ea3.chunk.js"
+  },
+  {
+    "revision": "a28ff7450f3c9dd26b41",
+    "url": "/DeathPulse274/react-components/static/js/runtime~main.c2772c80.js"
+  }
+]);
