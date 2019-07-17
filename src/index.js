8c8a1ef22069c2265501e5d7275380f7a1d9799a
santhosh274 <santhosh.k@rapyder.com>
2019-07-17 18:22:01 +0530

    Component Nesting passing child component from parent component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className = "ui container comments">
+            <br/>
+            <ApprovalCard>
+                <CommentDetail author={faker.name.firstName()} timeAgo="today at 9:00 PM"  comments={faker.random.words()} imageSource={faker.image.avatar()} />
+            </ApprovalCard>
+                
+            <ApprovalCard>
+            <CommentDetail author={faker.name.firstName()} timeAgo="yesterday at 4:00 PM" comments={faker.random.words()} imageSource={faker.image.avatar()} />
+            </ApprovalCard>
             <CommentDetail author={faker.name.firstName()} timeAgo="today at 9:00 PM"  comments={faker.random.words()} imageSource={faker.image.avatar()} />
             <CommentDetail author={faker.name.firstName()} timeAgo="yesterday at 4:00 PM" comments={faker.random.words()} imageSource={faker.image.avatar()} />
             <CommentDetail author={faker.name.firstName()} timeAgo="today at 10:00 AM" comments={faker.random.words()} imageSource={faker.image.avatar()}/>
